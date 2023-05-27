2f6bdeed40da69817e2dec756876cc63b1a8d58f
Akhad Mansurov <bakhadiroff5101@gmail.com>
2023-05-27 22:01:17 +0500

    first commit

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -80,22 +80,22 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'reservations',
-#         'HOST' : 'localhost',
-#         'PORT' : '3306',
-#         'USER' : 'root',
-#         'PASSWORD' : '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'mydatabase',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'reservations',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+        'USER' : 'root',
+        'PASSWORD' : '',
+    }
+}
 
 # The settings for media files have been updated for the Graded assessment
 MEDIA_URL = '/media/'
